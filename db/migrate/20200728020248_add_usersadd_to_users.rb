467668d48edc9e5f503eed802d8fd4fe7b440c14
vagrant <vagrant@localhost.localdomain>
2020-07-28 06:22:01 +0000

    Create login function

---
+++ db/migrate/20200728020248_add_usersadd_to_users.rb
@@ -0,0 +1,8 @@
+class AddUsersaddToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :introduction, :text
+    add_column :users, :goal, :string
+    add_column :users, :profile_image_id, :string
+  end
+end
