fba3782934125246b3a10d8bf7bee61ec00c5594
vagrant <vagrant@localhost.localdomain>
2020-08-05 01:18:42 +0000

    top layout fix

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,29 +1,27 @@
 class FavoritesController < ApplicationController
   def create
-  	article = Article.find(params[:article_id])
-    favorite = current_user.favorites.new(article_id: article.id)
+  	@article = Article.find(params[:article_id])
+    favorite = current_user.favorites.new(article_id: @article.id)
     favorite.save
-    redirect_to articles_path
   end
 
   def destroy
-  	article = Article.find(params[:article_id])
-    favorite = current_user.favorites.find_by(article_id: article.id)
+  	@article = Article.find(params[:article_id])
+    favorite = current_user.favorites.find_by(article_id: @article.id)
     favorite.destroy
-    redirect_to articles_path
   end
 
-  def favoritescreate
-    article = Article.find(params[:article_id])
-    favorite = current_user.favorites.new(article_id: article.id)
-    favorite.save
-    redirect_to article_path(params[:article_id])
-  end
+  # def favoritescreate
+  #   @article = Article.find(params[:article_id])
+  #   favorite = current_user.favorites.new(article_id: @article.id)
+  #   favorite.save
+  #   redirect_to article_path(params[:article_id])
+  # end
 
-  def favoritesdestroy
-    article = Article.find(params[:article_id])
-    favorite = current_user.favorites.find_by(article_id: article.id)
-    favorite.destroy
-    redirect_to article_path(params[:article_id])
-  end
+  # def favoritesdestroy
+  #   @article = Article.find(params[:article_id])
+  #   favorite = current_user.favorites.find_by(article_id: @article.id)
+  #   favorite.destroy
+  #   redirect_to article_path(params[:article_id])
+  # end
 end
